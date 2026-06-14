f6979e3bca758160551cef930ea3e58cd709c4bf
Ruvym <ruva2001@gmail.com>
2026-06-14 19:13:11 +0300

    feat: initialize root application module and health check controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { PlayerModule } from './player/player.module.js'
 import { StaminaModule } from './stamina/stamina.module.js'
+import { AppController } from './app.controller.js'
 
 @Module({
-  imports: [PlayerModule, StaminaModule]
+  imports: [PlayerModule, StaminaModule],
+  controllers: [AppController]
 })
 export class AppModule {}
 
